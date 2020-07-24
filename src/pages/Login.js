95f70fe26ff2e6b3f3bc8752af2379cb2b13609d
liuhao <liuhao@zgtozed.com>
2020-07-24 15:13:47 +0800

    更新

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,24 +1,37 @@
 import React, { useContext, useState, useEffect } from 'react'
 import { View, TouchableHighlight, TouchableWithoutFeedback, Text, TextInput, StyleSheet, Alert, ImageBackground } from 'react-native'
-import { Toast } from '@ant-design/react-native';
+import { Toast, ActivityIndicator } from '@ant-design/react-native';
 import { Context } from '../../App'
 import { changeLoginStateAction } from '../store/action/action'
 import { interfaces } from '../config/config'
 import fetchRequest from '../common/fetchRequest'
+
+let timer = null
 export default Login = ({ navigation }) => {
-    const context = useContext(Context)
+    const { state, dispatch } = useContext(Context)//登录状态
     const [phone, setPhone] = useState('')
     const [password, setPassword] = useState('')
     const [code, setCode] = useState('')
-    const [isRegister, setRegister] = useState(false)
+    const [isRegister, setRegister] = useState(false)//当前是否为注册页面
+    const [isSendCode, setSendCode] = useState(false)
+    const [codeText, setCodeText] = useState("获取验证码")//验证码按钮文字
+
+    useEffect(() => {
+        return () => {
+            clearInterval(timer)
+        };
+    }, [])
+
+
+    //登录
     function login() {
-        if (phone == '18877611836' && password == '321123aaa') {
+        if (phone != '1' && password != 'a') {
             Toast.loading('登录中...', 1, () => {
-                context.dispatch(changeLoginStateAction(true))
-                Toast.info("登录成功")
+                Toast.info({ content: "登录成功", duration: 1, mask: false })
+                dispatch(changeLoginStateAction(true))
             });
         } else {
-            Toast.info("密码错误")
+            Toast.info({ content: "密码错误", duration: 1, mask: false })
         }
         // fetchRequest(interfaces.LOGIN, 'POST', { openid:phone, password })
         //     .then(res => {
@@ -29,11 +42,35 @@ export default Login = ({ navigation }) => {
         //         Alert.alert('请求失败')
         //     })
     }
+
+    //获取验证码
     function sandCode() {
-        Toast.loading('加载中...', 1, () => {
-            Toast.info("发送成功")
-        });
+        setSendCode(true)
+        setTimeout(() => {
+            setSendCode(false)
+            countDown(60)
+            Toast.info({ content: "发送成功", duration: 1, mask: false })
+        }, 1000);
     }
+
+    //倒计时
+    function countDown(sec) {
+        timer = setInterval(() => {
+            setCodeText(sec);
+            sec--;
+            if (sec == "00") {
+                setCodeText("获取验证码");
+                clearInterval(timer);
+                return;
+            }
+            if (sec < 10) {
+                sec = "0" + sec;
+            }
+            setCodeText(sec);
+        }, 1000);
+    }
+
+
     return (
         <ImageBackground source={require('../assets/images/background.jpg')} style={{ width: '100%', height: '100%' }}>
             <View style={styles.content}>
@@ -53,14 +90,18 @@ export default Login = ({ navigation }) => {
                 {isRegister ? (
                     <View style={styles.code}>
                         <TextInput
-                            style={[styles.input, { width: 150, marginRight: 20 }]}
+                            style={[styles.input, { width: 120, marginRight: 20 }]}
                             onChangeText={text => setCode(text)}
                             value={code}
                             placeholder='验证码'
                         />
-                        <TouchableHighlight onPress={() => sandCode()} style={[styles.button, { width: 70, borderRadius: 4 }]} >
+                        <TouchableHighlight
+                            onPress={() => sandCode()}
+                            style={[styles.button, { width: 100, borderRadius: 4 }]}
+                            disabled={isSendCode}>
                             <View>
-                                <Text style={styles.button_text}>发送验证码</Text>
+                                {isSendCode ? <ActivityIndicator color="#fff" size="large" /> :
+                                    <Text style={styles.button_text}>{codeText}</Text>}
                             </View>
                         </TouchableHighlight>
                     </View>) : null
@@ -79,6 +120,8 @@ export default Login = ({ navigation }) => {
         </ImageBackground>
     )
 }
+
+
 const styles = StyleSheet.create({
     content: {
         flex: 1,
@@ -91,7 +134,8 @@ const styles = StyleSheet.create({
         borderColor: 'gray',
         borderWidth: 1,
         marginTop: 15,
-        backgroundColor: '#fff'
+        backgroundColor: '#fff',
+        borderRadius: 6
     },
     button: {
         marginTop: 15,
